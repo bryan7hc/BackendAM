3788b6b4ce8fecd0ce4985d05d211ae4cb768124
tomato <N00314241@upn.pe>
2025-07-09 00:47:56 -0500

    update db

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,24 @@
 import mysql from "mysql2/promise";
 
-const db = await mysql.createPool({
-  host: "automundo.mysql.database.azure.com",
-  user: "superadmin",
-  password: "Admin123",
-  database: "automundoDB2",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+async function initializeDatabase() {
+  const db = await mysql.createPool({
+    host: "automundo.mysql.database.azure.com",
+    user: "superadmin",
+    password: "Admin123",
+    database: "automundoDB2",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
 
-db.getConnection((err, connection) => {
-  if (err) {
-    console.error("Error de conexión con MySQL:", err);
-  } else {
+  try {
+    const connection = await db.getConnection();
     console.log("Conexión a la base de datos MySQL establecida (con pool)");
     connection.release();
+  } catch (err) {
+    console.error("Error de conexión con MySQL:", err);
   }
-});
+}
+
+initializeDatabase();
 export default db;
